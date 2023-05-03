b400f4da423758b7249b69d3f5e117c2a242b72b
Larisa76 <118682544+Larisa76@users.noreply.github.com>
2023-05-03 21:42:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ btn.addEventListener('click', () => {
     navList.classList.toggle('show-list')  
 })
 
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    serializeForm(applicantForm)
+  }
+  
+  const applicantForm = document.getElementById('mars-once')
+  applicantForm.addEventListener('submit', handleFormSubmit)
+  
+
+ 
+  function serializeForm(formNode) {
+    const { elements } = formNode
+    const data = Array.from(elements)
+      .filter((item) => !!item.name)
+      .map((element) => {
+        const { name, value } = element
+  
+        return { name, value }
+      })
+  
+    console.log(data)
+  }
